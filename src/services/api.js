dfe58db6693da0adff93f0968727ca9983f7738c
itsAbhirajSinha <mailabhiraj123@gmail.com>
2024-03-29 19:17:11 +0530

    testing frontend

--- src/services/api.js
+++ src/services/api.js
@@ -1,23 +1,12 @@
-import axios from "axios";
 
-export const getMeetToken = async (room, user) => {
-    try {
-        const params = {
-            "roomName": room,
-            "participantName": user
-        };
+import axios from "axios"
 
-        const res = await axios.get(`https://connectnow-backend.vercel.app/getToken`, {
-            params,
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-
-        return res.data;
-    } catch (error) {
-        // Handle error
-        console.error('Error fetching token:', error);
-        throw error; // Rethrow error for the caller to handle
+export const getMeetToken=(room,user)=>{
+   
+    const params={
+        "roomName":room,
+        "participantName":user
     }
-};
+    const res = axios.get(`https://connectnow-backend.vercel.app/getToken`,{params})
+    return res
+}
